4109c42d826e59626a53c3e17a8a7675231ef366
Yuya Ebihara <ebyhry@gmail.com>
2023-05-09 10:52:43 +0900

    Remove redundant analyzeCheckConstraints
    It causes duplicated constraints for UPDATE statement.

--- core/trino-main/src/main/java/io/trino/sql/analyzer/StatementAnalyzer.java
+++ core/trino-main/src/main/java/io/trino/sql/analyzer/StatementAnalyzer.java
@@ -2224,7 +2224,6 @@ class StatementAnalyzer
                     .withRelationType(RelationId.anonymous(), new RelationType(outputFields))
                     .build();
             analyzeFiltersAndMasks(table, targetTableName, new RelationType(outputFields), accessControlScope);
-            analyzeCheckConstraints(table, targetTableName, accessControlScope, tableSchema.getTableSchema().getCheckConstraints());
             analysis.registerTable(table, tableHandle, targetTableName, session.getIdentity().getUser(), accessControlScope);
 
             Scope tableScope = createAndAssignScope(table, scope, outputFields);
